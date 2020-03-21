38b39a45bc7dcdfcb2d85ddce736ff4e36a80862
Guillermo Williamson <gwilliamtam@gmail.com>
2020-03-21 18:58:45 -0500

    control wheels and arms

--- robot.py
+++ robot.py
@@ -20,10 +20,10 @@ def main():
 
     mouth = Mouth(face)
     eyes = Eyes(face)
-    text_area.display('ip', host.ip)
+    #text_area.display('ip', host.ip)
 
     mouth.small()
-    wheels.setSpeed(50)
+    wheels.setSpeed(150)
     ip_event = TimeEvent(60)
     test_event = TimeEvent(1)
     test_event.hold()
@@ -31,35 +31,54 @@ def main():
 
     blink_eyes = TimeEvent(7)
 
-    while TRUE:
+    in_loop = TRUE
+    while in_loop:
         k = face.key()
         if k != "":
-            print("Keypressed is ", k)
             if k == 'Up':
                 wheels.advanceCar()
+                eyes.look_down()
             elif k == 'Left':
                 wheels.turnLeft()
+                eyes.look_left()
             elif k == 'Right':
                 wheels.turnRight()
+                eyes.look_right()
             elif k == 'Down':
                 wheels.reverseCar()
-            elif k == 'Q':
-                wheels.turnOffMotors()
+                eyes.look_up()
+            elif k == 'Return':
+                wheels.stopCar()
+                eyes.look_center()
+            elif k >= '0' and k <='9':
+                speed_key = int(k)
+                if speed_key == 0:
+                    speed_key = 10
+                wheels.setSpeed(25*speed_key)
+            elif k == 'w':
+                arms.left_arm.arm_up()
+                arms.right_arm.arm_up()
+            elif k == 'a':
+                arms.left_arm.arm_down()
+                arms.right_arm.arm_up()
+            elif k == 's':
+                arms.left_arm.arm_middle()
+                arms.right_arm.arm_middle()
+            elif k == 'x':
+                arms.left_arm.arm_down()
+                arms.right_arm.arm_down()
+            elif k == 'd':
+                arms.left_arm.arm_up()
+                arms.right_arm.arm_down()
+            elif k == 'Escape':
+                wheels.stopCar()
+                in_loop = FALSE
                 break
         
-        if ip_event.interval():
-            text_area.remove('ip')
+        #if ip_event.interval():
+            #text_area.remove('ip')
 
         if blink_eyes.interval():
             eyes.blink_eyes()
-
-
-    face.win.getMouse()
-    face.win.close()
-
-
 main()
 
-
-atexit
-atexit.register(wheels.turnOffMotors)
