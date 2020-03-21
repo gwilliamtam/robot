794ef360def6d2ce62b5359400b3a23982b88472
Guillermo Williamson <gwilliamson@cinsay.com>
2020-03-21 16:30:22 -0500

    Work in progress

--- robot.py
+++ robot.py
@@ -11,7 +11,7 @@ import math
 import atexit
 
 
-def other():
+def main():
     wheels = Wheels()
     face = Face()
     host = Host()
@@ -108,25 +108,8 @@ def other():
     face.win.close()
 
 
-#main()
+main()
 
 
-#at exit
-#atexit.register(wheels.turnOffMotors)
-
-root = Tk()
-
-def key(event):
-    print("pressed", repr(event.char))
-    
-def callback(event):
-    frame.focus_set()
-    print("clicked at", event.x, event.y)
-    
-frame = Frame(root, width=100, height=100)
-frame.bind("<Key>", key)
-frame.bind("<Button-1>", callback)
-frame.pack()
-
-root.mainloop()
-    print("I am in a loop")
+atexit
+atexit.register(wheels.turnOffMotors)
