0a163ddb1effb43a2b3b0ab391fe235664f07ffb
Guillermo Williamson <gwilliamtam@gmail.com>
2020-03-24 21:42:00 -0500

    Add Obstacles

---
+++ Obstacles/obstacles.py
@@ -0,0 +1,12 @@
+from Distance.distance import *
+
+class Obstacles:
+    def __init__(self):
+        self.distance = Distance()
+        self.front_min_distance = 10 #cm
+        
+    def check_move_forward(self, wheels, text_area, buzzer):
+        if wheels.status == 'forward' and self.distance.get() < self.front_min_distance:
+            wheels.stopCar()
+            text_area.display('Obstacle')
+            buzzer.play('error')
