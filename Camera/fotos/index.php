253755ffb72f90aab70f1ca5df3a561e37f44d22
Guillermo Williamson <gwilliamson@Guillermos-Air.lan>
2020-06-20 03:01:10 -0500

    change reload foto

--- Camera/fotos/index.php
+++ Camera/fotos/index.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-<style>
-.foto {
-	width: 100%;
-}
-</style>
-<title>Robowilly Fotos</title>
+    <style>
+        .foto {
+            width: 100%;
+        }
+    </style>
+    <title>Robowilly Fotos</title>
 </head>
 <body>
-<img class='foto' src='shoot_and_show.gif'>
+<img class='foto' src='shoot_and_show.gif' id="myImage">
 <script>
-setTimeout(function() { refresh_page() }, 5000); 
-function refresh_page() { window.location.reload(true); }
+    setInterval(function() {
+        var myImageElement = document.getElementById('myImage');
+        myImageElement.src = 'shoot_and_show.gif?rand=' + Math.random();
+    }, 5000);
 </script>
 </body>
 </html>
